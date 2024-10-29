27d899e7ea00c6734b79d850326ffe4a18ca2a19
truondin <dannenr@gmail.com>
2024-10-29 12:12:35 +0200

    Exercise 3 - task 1 add

---
+++ exercise3/task1/task1_square_movement.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for (( i = 0; i < 4; i++ )); do
+  # Move forward
+  ros2 topic pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"  &&
+
+  # Turn
+  ros2 topic pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 1.57}}"  &&
+  sleep 1
+done
+
